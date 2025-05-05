e05045920287876fe0c97f053a1f5c91b0f2c73e
Bartłomiej Rzońca <121831849+Rzoncik@users.noreply.github.com>
2025-05-05 18:38:39 +0200

    Delete AplikacjaBankowa directory

--- AplikacjaBankowa/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AplikacjaBankowa.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
