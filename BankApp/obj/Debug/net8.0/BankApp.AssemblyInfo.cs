817f09a6791ba66c8d0889343b5ae862ac758afc
Bartłomiej Rzońca <121831849+Rzoncik@users.noreply.github.com>
2025-06-06 09:58:49 +0200

    prepared database

--- BankApp/obj/Debug/net8.0/BankApp.AssemblyInfo.cs
+++ BankApp/obj/Debug/net8.0/BankApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88fd7566b88abeb5b3d361c00ae67061640ba0dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4abd61ac1f4c2646866f59db189b909d28aa2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
