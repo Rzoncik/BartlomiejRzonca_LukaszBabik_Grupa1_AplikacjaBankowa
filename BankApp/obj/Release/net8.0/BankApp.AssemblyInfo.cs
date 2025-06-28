8ec60319ddbc09dfd2337f466e1b4363e40fd4de
Bartłomiej Rzońca <121831849+Rzoncik@users.noreply.github.com>
2025-06-28 11:05:56 +0200

    Small design fixes

--- BankApp/obj/Release/net8.0/BankApp.AssemblyInfo.cs
+++ BankApp/obj/Release/net8.0/BankApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece75f8b07e57a56cac9e3f0f71824b509830fe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1b35ffeb1643936f20a89ce06e09e06c737af9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
